c1fc10f8ade55a0a039c8c2fa0aed3ee60f9e460
lAkrill <kavrusevich2002@gmail.com>
2025-09-21 09:39:12 +0300

    fix versioning

--- prod/backend/WebApp/DTO/Common/VersionsDTO.cs
+++ prod/backend/WebApp/DTO/Common/VersionsDTO.cs
@@ -1,7 +1,12 @@
 namespace WebApp.DTO.Common;
 
+using System.Text.Json.Serialization;
+
 public class VersionsDTO
 {
+    [JsonPropertyName("backend")]
     public string Backend { get; set; } = string.Empty;
+
+    [JsonPropertyName("frontend")]
     public string Frontend { get; set; } = string.Empty;
 }
